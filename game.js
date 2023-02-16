445a2012d01b5c8117b83216aee56aa2fecd552b
chapupa <ryanisamu@gmail.com>
2023-02-16 12:25:51 -0800

    Add README.md and game.js and r-p-s.html

--- game.js
+++ game.js
@@ -1,35 +1,68 @@
         
-
+        let humanScore = 0;
+        let computerScore = 0;
+        let tieGame = 0;
         
 
         function getComputerChoice () {
-            let randomNumber = Math.floor(Math.random() * 3);
+            let randomNumber = Math.floor(Math.random() * 3) + 1;
             switch (randomNumber) {
-            case 0:
+            case 1:
             return "rock";
             break;
-            case 1:
+            case 2:
             return "scissors";
             break;
-            case 2:
+            case 3:
             return "paper";
             break;
             };
         }
-            console.log(getComputerChoice());
-
-        const playerSelection = "rock";
-        const computerSelection = getComputerChoice();
 
         function playRound(playerSelection, computerSelection) {
-            if (playerSelection > computerSelection) {
-                return "You Won!"
-            } else if (playerSelection < computerSelection) {
-                return "You Lost."
-            } else {
-                return "You Tied."
+            if (playerSelection === 'rock' && computerSelection === 'scissors') {
+                humanScore += 1;
+                return `${playerSelection} beats ${computerSelection}! You Won!`;
+            } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+                computerScore += 1;
+                return `${playerSelection} loses to ${computerSelection}. You Lost.`;
             }
+            
+            if (playerSelection === 'scissors' && computerSelection === 'paper') {
+                humanScore += 1;
+                return `${playerSelection} beats ${computerSelection}! You Won!`;
+            } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+                computerScore += 1;
+                return `${playerSelection} loses to ${computerSelection}. You Lost.`;
+            }
+
+            if (playerSelection === 'paper' && computerSelection === 'rock') {
+                humanScore += 1;
+                return `${playerSelection} beats ${computerSelection}! You Won!`;
+                } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+                computerScore += 1;
+                return `${playerSelection} loses to ${computerSelection}. You Lost.`;
+                }
+
+            if (playerSelection === computerSelection) {
+                tieGame += 1;
+                return "It's a tie.";
+                } 
+            }
+        
+            let computerSelection = getComputerChoice();
+
+        function game () {
+            for (let i = 0; i < 5; i++) {
+            let question = prompt('Rock, Paper, or Scissors?');
+            playerSelection = question; 
+            question = question.toLowerCase;   
+            computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
+            }
+        return `You won ${humanScore} game(s), the computer won ${computerScore} game(s) and you tied ${tieGame} out of 10 games.`
         }
+        game();
+        alert(game());
 
         
-        console.log(playRound(playerSelection, computerSelection));
